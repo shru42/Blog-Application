db3b9787d11ee8b4c500977bedea06d9fc5c826c
shru42 <157052479+shru42@users.noreply.github.com>
2024-07-27 19:07:52 +0530

    Delete blogApplication/src/main/java/com/example/blogApplication directory

--- blogApplication/src/main/java/com/example/blogApplication/repository/CommentRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.blogApplication.repository;
-
-import com.example.blogApplication.entity.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepo extends JpaRepository<Comment,Integer> {
-}
